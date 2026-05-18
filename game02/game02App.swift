db71a62f2f87b2c112c65b3cb8e71748331ed3a9
WJN <sandy0981667515@gmail.com>
2026-05-18 16:42:40 +0800

    Initial Commit

---
+++ game02/game02App.swift
@@ -0,0 +1,17 @@
+//
+//  game02App.swift
+//  game02
+//
+//  Created by wjn on 2026/5/18.
+//
+
+import SwiftUI
+
+@main
+struct game02App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
